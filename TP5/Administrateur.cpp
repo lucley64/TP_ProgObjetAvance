7c8687bef2b486008536d2fc6522109fc9ea3c02
Luc Leydert <luc.leydert@gmail.com>
2021-11-27 16:06:39 +0100

    Implémentation de la classe Administrateur

--- TP5/Administrateur.cpp
+++ TP5/Administrateur.cpp
@@ -1,1 +1,19 @@
 #include "Administrateur.h"
+
+Administrateur::Administrateur(string identite, string fonction) :
+	identite(identite),
+	fonction(fonction) {}
+
+Administrateur::Administrateur(const Administrateur& admin) :
+	identite(admin.identite),
+	fonction(admin.fonction) {}
+
+Administrateur::~Administrateur() {}
+
+Administrateur& Administrateur::operator=(const Administrateur& admin) {
+	identite = admin.identite;
+	fonction = admin.fonction;
+
+	return *this;
+}
+
--- TP5/Administrateur.h
+++ TP5/Administrateur.h
@@ -1,8 +1,21 @@
 #ifndef ADMINISTRATEUR_H
 #define ADMINISTRATEUR_H
 
-class Administrateur {
+#include <iostream>
 
+using namespace std;
+
+class Administrateur {
+public:
+	string identite;
+	string fonction;
+
+	Administrateur(string identite="", string fonction="");
+	Administrateur(const Administrateur& admin);
+
+	virtual ~Administrateur();
+
+	Administrateur& operator=(const Administrateur& admin);
 };
 
 #endif // !ADMINISTRATEUR_H
